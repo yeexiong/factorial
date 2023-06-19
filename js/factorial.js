e92897ed26eb54afd87141df65dbc8d30df5bd2a
EC2 Default User <yeexiong@users.noreply.github.com>
2023-06-19 15:40:57 +0000

    factorial finished

--- js/factorial.js
+++ js/factorial.js
@@ -1,2 +1,28 @@
 exports.factorial = function(num) {
+    
+    // create an array to store the values of input Num
+    let myArrayNum = []
+    
+    // store the values that come before num + 1
+    for (let i = 1; i < num + 1; i++) {
+        myArrayNum.push(i);
+        
+    }
+    
+    // initialize the variable with a value of 1 so that the factorial numbers aren't multiplied with 0
+    let factorial = 1;
+
+    // Calculate the factorial by multiplying all the numbers in the array
+    for (let i = 0; i < myArrayNum.length; i++) {
+    factorial *= myArrayNum[i];
+  }
+    // code check
+    console.log(factorial);
+    
+    // function must return something
+    return factorial;
+    
 };
+
+// call method with argument passed for parameter num
+exports.factorial(5)
